b1bfaf6ea5580fd63118d2c125c53a988194c35d
YangOu <taurusou@gmail.com>
2026-04-04 22:26:57 +0000

    connect database to flask

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from --Yang Ou-- in 3308.'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://yang_ou_postgres_user:Lmfaixi91SgYxM5XesWl5lEz91RJ2mNx@dpg-d78okfia214c73acrhgg-a/yang_ou_postgres")
+    conn.close()
+    return "Database Connection Successful."
