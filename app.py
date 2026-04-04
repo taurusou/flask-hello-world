c9b7eedd2333629bd486ce23db3713acd358bf54
YangOu <taurusou@gmail.com>
2026-04-04 23:33:42 +0000

    create a table

--- app.py
+++ app.py
@@ -11,3 +11,19 @@ def testing():
     conn = psycopg2.connect("postgresql://yang_ou_postgres_user:Lmfaixi91SgYxM5XesWl5lEz91RJ2mNx@dpg-d78okfia214c73acrhgg-a/yang_ou_postgres")
     conn.close()
     return 'Database Connection Successful.'
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://yang_ou_postgres_user:Lmfaixi91SgYxM5XesWl5lEz91RJ2mNx@dpg-d78okfia214c73acrhgg-a/yang_ou_postgres")
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
