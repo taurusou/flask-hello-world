ff7005ed9d2498ac2a730547303329451b641f53
YangOu <taurusou@gmail.com>
2026-04-05 01:46:19 +0000

    add db_select

--- app.py
+++ app.py
@@ -44,3 +44,23 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://yang_ou_postgres_user:Lmfaixi91SgYxM5XesWl5lEz91RJ2mNx@dpg-d78okfia214c73acrhgg-a/yang_ou_postgres")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fechall()
+    conn.close()
+    response_string = ""
+    response_string +="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string += "<td>{}</td>.format(info)
+        response_string +="</tr>"
+    response_string += "</table>"
+    return response_string
+    
